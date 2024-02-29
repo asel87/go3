70d6d38f02f8fd13e778956d066e9021eb856baa
asel87 <146825882+asel87@users.noreply.github.com>
2024-03-01 00:53:23 +0600

    Update contact.go

--- goassik3/cleanArchitecture/services/contact/internal/repository/storage/postgres/contact.go
+++ goassik3/cleanArchitecture/services/contact/internal/repository/storage/postgres/contact.go
@@ -1,19 +1,60 @@
 package postgres
 
-import "assik3/services/contact/internal/domain/contact"
+import (
+	"assik3/services/contact/internal/domain/contact"
+	"database/sql"
+	"errors"
+	"fmt"
+)
 
 func (r *Repository) CreateContact(contact *contact.Contact) (*contact.Contact, error) {
-	panic("implement later")
+	stmt := "INSERT INTO contacts (phoneNumber, name, surname, patronymic) VALUES(?, ?, ?, ?)"
+
+	_, err := r.db.Exec(stmt, contact.PhoneNumber(), contact.Name(), contact.Surname(), contact.Patronymic())
+	if err != nil {
+		return nil, err
+	}
+
+	return contact, nil
 }
 
 func (r *Repository) ReadContactByID(contactID int) (*contact.Contact, error) {
-	panic("implement later")
+	stmt := "SELECT id, phoneNumber, name, surname, patronymic FROM contacts WHERE id = ?"
+
+	row := r.db.QueryRow(stmt, contactID)
+
+	var c contact.Contact
+
+	err := row.Scan(c.ID(), c.PhoneNumber(), c.Name(), c.Surname(), c.Patronymic())
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return &contact.Contact{}, fmt.Errorf("no rows found")
+		} else {
+			return &contact.Contact{}, err
+		}
+	}
+
+	return &c, nil
 }
 
 func (r *Repository) UpdateContact(contactID int, contact *contact.Contact) (*contact.Contact, error) {
-	panic("implement later")
+	stmt := `UPDATE contacts SET phoneNumber = ?, name = ?, surname = ?, patronymic = ? WHERE id = ?`
+
+	_, err := r.db.Exec(stmt, contact.PhoneNumber(), contact.Name(), contact.Surname(), contact.Patronymic(), contactID)
+	if err != nil {
+		return nil, err
+	}
+
+	return contact, nil
 }
 
 func (r *Repository) DeleteContact(contactID int) error {
-	panic("implement later")
+	stmt := "DELETE FROM contacts WHERE id = ?"
+
+	_, err := r.db.Exec(stmt, contactID)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
