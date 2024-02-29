6809da390583508cd32999a80504f69693bdf0e8
asel87 <146825882+asel87@users.noreply.github.com>
2024-03-01 00:54:15 +0600

    Update contactInGroup.go

--- goassik3/cleanArchitecture/services/contact/internal/repository/storage/postgres/contactInGroup.go
+++ goassik3/cleanArchitecture/services/contact/internal/repository/storage/postgres/contactInGroup.go
@@ -5,13 +5,41 @@ import (
 )
 
 func (r *Repository) CreateContantIntoGroup(groupID int, contact *contact.Contact) (*contact.Contact, error) {
-	panic("implement later")
+	stmt := "INSERT INTO contacts (phoneNumber, name, surname, patronymic) VALUES(?, ?, ?, ?)"
+
+	_, err := r.db.Exec(stmt, contact.PhoneNumber(), contact.Name(), contact.Surname(), contact.Patronymic())
+	if err != nil {
+		return nil, err
+	}
+
+	stmt = "INSERT INTO contactGroup (contactId, groupId) VALUES(?, ?)"
+
+	_, err = r.db.Exec(stmt, contact.ID(), groupID)
+	if err != nil {
+		return nil, err
+	}
+
+	return contact, nil
 }
 
 func (r *Repository) AddContactToGroup(groupID int, contactID int) error {
-	panic("implement later")
+	stmt := "INSERT INTO contactGroup (contactId, groupId) VALUES(?, ?)"
+
+	_, err := r.db.Exec(stmt, contactID, groupID)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (r *Repository) DeleteContantFromGroup(groupID int, contactID int) error {
-	panic("implement later")
+	stmt := "DELETE FROM contactGroup WHERE contactId = ? AND groupId = ?"
+
+	_, err := r.db.Exec(stmt, contactID, groupID)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
