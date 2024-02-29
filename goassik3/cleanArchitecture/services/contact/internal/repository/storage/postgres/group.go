bdd6d9c7d8bb9c7cc392235735ac0a3ca6cb17ae
asel87 <146825882+asel87@users.noreply.github.com>
2024-03-01 00:55:08 +0600

    Update group.go

--- goassik3/cleanArchitecture/services/contact/internal/repository/storage/postgres/group.go
+++ goassik3/cleanArchitecture/services/contact/internal/repository/storage/postgres/group.go
@@ -1,19 +1,59 @@
-package postgres
 
-import "assik3/services/contact/internal/domain/group"
+import (
+	"assik3/services/contact/internal/domain/group"
+	"database/sql"
+	"errors"
+	"fmt"
+)
 
 func (r *Repository) CreateGroup(group *group.Group) (*group.Group, error) {
-	panic("implement later")
+	stmt := "INSERT INTO groups (name) VALUES(?)"
+
+	_, err := r.db.Exec(stmt, group.Name())
+	if err != nil {
+		return nil, err
+	}
+
+	return group, nil
 }
 
 func (r *Repository) ReadGroupByID(groupID int) (*group.Group, error) {
-	panic("implement later")
+	stmt := "SELECT id, name FROM groups WHERE id = ?"
+
+	row := r.db.QueryRow(stmt, groupID)
+
+	var g group.Group
+
+	err := row.Scan(g.ID(), g.Name())
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return &group.Group{}, fmt.Errorf("no rows found")
+		} else {
+			return &group.Group{}, err
+		}
+	}
+
+	return &g, nil
 }
 
 func (r *Repository) UpdateGroup(groupID int, group *group.Group) (*group.Group, error) {
-	panic("implement later")
+	stmt := `UPDATE groups SET name = ? WHERE id = ?`
+
+	_, err := r.db.Exec(stmt, group.Name(), groupID)
+	if err != nil {
+		return nil, err
+	}
+
+	return group, nil
 }
 
 func (r *Repository) DeleteGroup(groupID int) error {
-	panic("implement later")
+	stmt := "DELETE FROM groups WHERE id = ?"
+
+	_, err := r.db.Exec(stmt, groupID)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
